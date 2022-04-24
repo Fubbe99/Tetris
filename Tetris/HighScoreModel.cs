3da56399fe5f997ff75c2d5bbf026e99f68caa0a
Fubbe99 <chrijor@gmail.com>
2022-04-24 20:29:35 +0200

    Added Player name change, Highscore list and Instructions

---
+++ Tetris/HighScoreModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    public class HighScoreModel
+    {
+        public HighScoreModel(string name, int score)
+        {
+            Name = name;
+            Score = score;
+        }
+
+        public string Name { get; set; }
+        public int Score { get; set; }
+    }
+}
